3b050d6afe62c28eba4684db088e39ce85f8eaf6
Kevin Miller <kemiller@csumb.edu>
2011-06-29 18:59:26 -0700

    Added check in node_delete to remove sid of webform results when a signup node is deleted.

--- modules/s4_features/s4_pages/overview_paragraph.tpl.php
+++ modules/s4_features/s4_pages/overview_paragraph.tpl.php
@@ -1,4 +1,10 @@
 <p class="overview">
-Together, we have placed <span class="number"><?php print $data['students']; ?></span> students with <span class="number"><?php print $data['sites']; ?></span> sites in <span class="number"><?php print $data['cities']; ?></span> different cities.
+Together, we 
+<?php if($data['students']): ?>
+  have placed <span class="number"><?php print $data['students']; ?></span> students
+<?php else: ?>
+  will place students
+<?php endif; ?>
+ with <span class="number"><?php print $data['sites']; ?></span> <?php print format_plural($data['sites'], 'site', 'sites'); ?> in <span class="number"><?php print $data['cities']; ?></span> <?php print format_plural($data['cities'], 'city', 'different cities'); ?>.
 </p>
 <p class="intro"><strong>See where we work:</strong> you can <?php print l('search a list', 'site-list'); ?> or <?php print l('view a map', 'map'); ?>.</p>
