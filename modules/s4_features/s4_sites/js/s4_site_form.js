eef35d04d5428e1219eddfe96db5f9be9c4d2c98
Kevin Miller <kemiller@csumb.edu>
2011-07-16 11:52:42 -0700

    Removed the contact records tab, added JS validation to the population total.

---
+++ modules/s4_features/s4_sites/js/s4_site_form.js
@@ -0,0 +1,29 @@
+(function($) {
+	$(document).ready(function () {
+		$('.field-name-field-site-demographics table tbody').append('<tr><td>Total percent</td><td id="demographic-total"></td></tr>');
+		$('.field-name-field-site-demographics input[type=text]').keyup(function() {
+			var total = 0;
+			$('.field-name-field-site-demographics input[type=text]').each(function() {
+				if(!isNaN(parseFloat($(this).val())) && isFinite($(this).val())) {
+					$(this).removeClass('error');
+					total += parseFloat($(this).val());
+				}
+				else {
+					if($(this).val().trim() != '') {
+						$(this).addClass('error');
+					}
+					else {
+						$(this).removeClass('error');
+					}
+				}
+			});
+			$('#demographic-total').html(total + '%');
+			if(total > 100) {
+				$('#demographic-total').addClass('error');
+			}
+			else {
+				$('#demographic-total').removeClass('error');
+			}
+		});
+	});
+})(jQuery);
