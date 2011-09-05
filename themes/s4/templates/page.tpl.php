593f1ab171858655a8f08199eb2794d904ce46dd
Kevin Miller <kemiller@csumb.edu>
2011-09-05 09:49:23 -0700

    Made page footer hide if there's no content and also removed the "Powered by S4 Project" message.

--- themes/s4/templates/page.tpl.php
+++ themes/s4/templates/page.tpl.php
@@ -117,29 +117,30 @@
 </div>
 <?php endif; ?>
 
-<div id="footer">
-	<div class="container">
-		<?php if($page['footer_left']): ?>
-			<div class="triptych">
-				<?php print render($page['footer_left']); ?>
+<?php if($page['footer_left'] || $page['footer_middle'] || $page['footer_right'] || $page['footer_bottom']): ?>
+	<div id="footer">
+		<div class="container">
+			<?php if($page['footer_left']): ?>
+				<div class="triptych">
+					<?php print render($page['footer_left']); ?>
+				</div>
+			<?php endif; ?>
+			<?php if($page['footer_middle']): ?>
+				<div class="triptych margin">
+					<?php print render($page['footer_middle']); ?>
+				</div>
+			<?php endif; ?>
+			<?php if($page['footer_right']): ?>
+				<div class="triptych margin">
+					<?php print render($page['footer_right']); ?>
+				</div>
+			<?php endif; ?>
+			<div class="clear"></div>
+			<?php if($page['footer_bottom']): ?>
+				<?php print render($page['footer_bottom']); ?>
+			<?php endif; ?>
+			<div class="clear">
 			</div>
-		<?php endif; ?>
-		<?php if($page['footer_middle']): ?>
-			<div class="triptych margin">
-				<?php print render($page['footer_middle']); ?>
-			</div>
-		<?php endif; ?>
-		<?php if($page['footer_right']): ?>
-			<div class="triptych margin">
-				<?php print render($page['footer_right']); ?>
-			</div>
-		<?php endif; ?>
-		<div class="clear"></div>
-		<?php if($page['footer_bottom']): ?>
-			<?php print render($page['footer_bottom']); ?>
-		<?php endif; ?>
-		<a href="http://s4.csumb.edu">Powered by the S4 Project</a>
-		<div class="clear">
 		</div>
 	</div>
-</div>
+<?php endif; ?>
