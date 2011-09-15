0885b0cd337c5efab92a59159391e9d5ce2e74c8
Kevin Miller <kemiller@csumb.edu>
2011-09-15 08:05:50 -0700

    Moved views saved searches to a fancybox

--- modules/s4_features/s4_reporting/js/s4_reporting.views.js
+++ modules/s4_features/s4_reporting/js/s4_reporting.views.js
@@ -2,16 +2,19 @@
 	Drupal.behaviors.s4_reporting_views = {
 		
 		attach : function() {
-			$('#show-fields').fancybox({
-				'onClosed' : function() {
-					$('#field-selector').addClass('element-invisible')
-										.parent('div')
-										.addClass('element-invisible');
-				},
-				'onStart'  : function() {
-					$('#field-selector').removeClass('element-invisible');
-				},
-				'autoDimensions' : false
+			$('a.show-form-fancybox').each(function() {
+				var $link = $(this);
+				$link.fancybox({
+					'onClosed' : function() {
+						$($link.attr('data-selector')).addClass('element-invisible')
+											.parent('div')
+											.addClass('element-invisible');
+					},
+					'onStart'  : function() {
+						$($link.attr('data-selector')).removeClass('element-invisible');
+					},
+					'autoDimensions' : false
+				});
 			});
 		}
 	};
