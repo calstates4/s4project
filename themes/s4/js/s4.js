eb4689ed74879b09498e7b4759e3568a01613731
Kevin Miller <kemiller@csumb.edu>
2011-10-05 17:23:02 -0700

    Removed auto-hiding bookmark flag links.

--- themes/s4/js/s4.js
+++ themes/s4/js/s4.js
@@ -2,23 +2,6 @@
 	$(document).ready(function() {
 		$('#search input[type=text]').placeholder();
 		
-		/** BINDING THE TOOLS SLIDE **/
-		
-		$('.bookmarks .flag-bookmarks').each(function() {
-			$(this).hide();
-		});
-		$('.bookmarks li a').focus(function() {
-			$('.bookmarks .flag-bookmarks').show();
-		});
-		$('.bookmarks li a').focusout(function() {
-			$('.bookmarks .flag-bookmarks').hide();
-		});
-		$('.bookmarks li').hover(function() {
-			$(this).find('.flag-bookmarks').show();
-		},
-		function() {
-			$(this).find('.flag-bookmarks').hide();
-		});
 		$('#footer-map .map').css('height', $('#footer-map').height() + 'px');
 	});
 })(jQuery)
