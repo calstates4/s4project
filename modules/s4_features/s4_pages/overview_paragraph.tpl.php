d00e9f9faf86a5efa138bb6b134211ad97be731e
Kevin Miller <kemiller@csumb.edu>
2012-06-05 06:39:36 -0700

    Allowed the overview paragraph to be rewritten with tokens. Also made the link to register a site respect user permissions.

--- modules/s4_features/s4_pages/overview_paragraph.tpl.php
+++ modules/s4_features/s4_pages/overview_paragraph.tpl.php
@@ -1,10 +1,6 @@
 <p class="overview">
-Together, we 
-<?php if ($data['students']): ?>
-  have placed <span class="number"><?php print $data['students']; ?></span> students
-<?php else: ?>
-  will place students
-<?php endif; ?>
- with <span class="number"><?php print $data['sites']; ?></span> <?php print format_plural($data['sites'], 'site', 'sites'); ?> in <span class="number"><?php print $data['cities']; ?></span> <?php print format_plural($data['cities'], 'city', 'different cities'); ?>.
+<?php print $paragraph; ?>
 </p>
+<?php if($registration): ?>
 <p class="intro">Would you like to join our program? <?php print l('Let us know about your organization.', 'sites/propose'); ?> </p>
+<?php endif; ?>
