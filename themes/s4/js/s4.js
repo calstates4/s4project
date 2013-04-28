01413c187c2fe4f5dc4de02b2f31622344af6040
Kevin Miller <kemiller@csumb.edu>
2013-04-28 11:26:08 -0700

    Removed reference to jquery sticky.

--- themes/s4/js/s4.js
+++ themes/s4/js/s4.js
@@ -2,8 +2,6 @@
 	$(document).ready(function() {
 		$('#search input[type=text]').placeholder();
 		
-		$('.sticky, .site-list .region-content-right').sticky({topSpacing:0});
-		
 		$('#footer-map .map').css('height', $('#footer-map').height() + 'px');
 	});
 })(jQuery)
