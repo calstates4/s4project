72a27315f416c90920806fc2c1f24237aadbc7e3
Kevin Miller <kemiller@csumb.edu>
2013-05-03 13:59:55 -0700

    Resize editor panes.

--- themes/s4/js/s4.js
+++ themes/s4/js/s4.js
@@ -3,5 +3,12 @@
 		$('#search input[type=text]').placeholder();
 		
 		$('#footer-map .map').css('height', $('#footer-map').height() + 'px');
+		
+		if($('.horizontal-tabs-panes').length) {
+			$(window).resize(function() {
+				$('.horizontal-tabs-panes').find('fieldset').css('width', ($('.horizontal-tabs-panes').width() - 28) + 'px');
+			});
+			$(window).trigger('resize');
+		}
 	});
 })(jQuery)
