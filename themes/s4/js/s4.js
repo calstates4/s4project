6af670149d755f19b21254cd101911070a0a01c3
Kevin Miller <kemiller@csumb.edu>
2013-05-09 12:45:51 -0700

    Fixed issue with progress bar.

--- themes/s4/js/s4.js
+++ themes/s4/js/s4.js
@@ -29,4 +29,79 @@
 	                    '</div>' +
 	                    '<div class="message">&nbsp;</div>');
 	};
+	
+	Drupal.progressBar.prototype.setProgress = function (percentage, message) {
+	  if (percentage >= 0 && percentage <= 100) {
+	    $('div.filled', this.element).css('width', percentage + '%');
+	    $('div.percentage', this.element).html(percentage + '%');
+	  }
+	  $('div.message', this.element).html(message);
+	  if (this.updateCallback) {
+	    this.updateCallback(percentage, message, this);
+	  }
+	};
+	
+	/**
+	 * Start monitoring progress via Ajax.
+	 */
+	Drupal.progressBar.prototype.startMonitoring = function (uri, delay) {
+	  this.delay = delay;
+	  this.uri = uri;
+	  this.sendPing();
+	};
+	
+	/**
+	 * Stop monitoring progress via Ajax.
+	 */
+	Drupal.progressBar.prototype.stopMonitoring = function () {
+	  clearTimeout(this.timer);
+	  // This allows monitoring to be stopped from within the callback.
+	  this.uri = null;
+	};
+	
+	/**
+	 * Request progress data from server.
+	 */
+	Drupal.progressBar.prototype.sendPing = function () {
+	  if (this.timer) {
+	    clearTimeout(this.timer);
+	  }
+	  if (this.uri) {
+	    var pb = this;
+	    // When doing a post request, you need non-null data. Otherwise a
+	    // HTTP 411 or HTTP 406 (with Apache mod_security) error may result.
+	    $.ajax({
+	      type: this.method,
+	      url: this.uri,
+	      data: '',
+	      dataType: 'json',
+	      success: function (progress) {
+	        // Display errors.
+	        if (progress.status == 0) {
+	          pb.displayError(progress.data);
+	          return;
+	        }
+	        // Update display.
+	        pb.setProgress(progress.percentage, progress.message);
+	        // Schedule next timer.
+	        pb.timer = setTimeout(function () { pb.sendPing(); }, pb.delay);
+	      },
+	      error: function (xmlhttp) {
+	        pb.displayError(Drupal.ajaxError(xmlhttp, pb.uri));
+	      }
+	    });
+	  }
+	};
+	
+	/**
+	 * Display errors on the page.
+	 */
+	Drupal.progressBar.prototype.displayError = function (string) {
+	  var error = $('<div class="messages error"></div>').html(string);
+	  $(this.element).before(error).hide();
+	
+	  if (this.errorCallback) {
+	    this.errorCallback(this);
+	  }
+	};
 })(jQuery)
