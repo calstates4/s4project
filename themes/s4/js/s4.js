6efdfef3f9183c5643d4bc915965df373d3c3eb9
Kevin Miller <kemiller@csumb.edu>
2013-07-27 09:52:02 -0700

    Fixed width issues with fields in site form.

--- themes/s4/js/s4.js
+++ themes/s4/js/s4.js
@@ -7,8 +7,8 @@
 			$('#footer-map .map').css('height', $('#footer-map').height() + 'px');
 			
 			if($('.horizontal-tabs-panes').length) {
-				$(window).resize(function() {
-					$('.horizontal-tabs-panes').find('fieldset:not(table fieldset), table').css('width', ($('.horizontal-tabs-panes').width() - 28) + 'px');
+				$(window).on('resize', function() {
+					$('.horizontal-tabs-panes').find('fieldset:not(table fieldset), table, .fieldset-wrapper:not(table .fieldset-wrapper)').css('width', ($('.horizontal-tabs-panes').width() - 28) + 'px');
 				});
 				$(window).trigger('resize');
 			}
